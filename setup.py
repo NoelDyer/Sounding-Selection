76f48345cabd19595de4468363c5fd5c341bcabd
Noel Dyer <ndyer214@gmail.com>
2022-01-04 20:10:46 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+import pathlib
+
+cwd = pathlib.Path(__file__).parent.resolve()
+long_description = (cwd / 'README.md').read_text(encoding='utf-8')
+
+setup(name='soundingselection',
+      version='1.0.0',
+      description='Label-Based Method for Hydrographic Sounding Selection',
+      license='MIT',
+      long_description=long_description,
+      author='Noel Dyer',
+      package_dir={'': 'src/soundingselection'},
+      install_requires=['triangle',
+                        'numpy==1.21.5',
+                        'shapely==1.8.0'],
+      python_requires='>=3.6, <4',
+      url='https://github.com/NoelDyer/Hydrographic-Sounding-Selection',
+      long_description_content_type='text/markdown',
+      zip_safe=True,
+      py_modules=['characterdimensions',
+                  'domain',
+                  'logger',
+                  'main',
+                  'node',
+                  'point',
+                  'pointset',
+                  'reader',
+                  'tin',
+                  'tree',
+                  'triangles',
+                  'validation',
+                  'vertex',
+                  'writer'],
+      entry_points={'console_scripts': ['soundingselection=main:main']}
+      )
