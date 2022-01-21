0880de468eb85d57f2d6aea695f9dd7a77d36656
Noel Dyer <ndyer214@gmail.com>
2022-01-21 18:08:46 -0500

    Update triangles.py

--- src/sounding_selection/triangles.py
+++ src/sounding_selection/triangles.py
@@ -1,5 +1,5 @@
 class Triangle(object):
-    """ A Triangle is encoded as the triple of the indexes of its vertices in the Vertex array"""
+    """ A Triangle is encoded as the triple of the indexes of its vertices in the Vertex array. """
     def __init__(self, v1, v2, v3):
         self.__v_ids = [v1, v2, v3]
         self.__visit = False
@@ -20,4 +20,4 @@ class Triangle(object):
         return 3
 
     def __str__(self):
-        return "%s %s %s"%(self.__v_ids[0],self.__v_ids[1],self.__v_ids[2])
+        return "%s %s %s" % (self.__v_ids[0], self.__v_ids[1], self.__v_ids[2])
