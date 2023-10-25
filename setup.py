5f665365022efb9774fadc3678057d5662f85a0a
Noel Dyer <ndyer214@gmail.com>
2023-10-25 09:41:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (cwd / 'README.md').read_text(encoding='utf-8')
 
 setup(name='sounding_selection',
       version='1.0.0',
-      description='Label-Based Method for Hydrographic Sounding Selection',
+      description='Chart Features, Data Quality, and Scale in Cartographic Sounding Selection from Composite Bathymetric Data',
       license='MIT',
       long_description=long_description,
       author='Noel Dyer',
